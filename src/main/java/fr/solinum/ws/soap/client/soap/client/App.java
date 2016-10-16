ff5cb89c7d8077efdf0ef6e2e5202e521641fdb2
abdessalem <belhadj.abdessalem@gmail.com>
2016-10-17 00:16:24 +0200

    add same fix

--- src/main/java/fr/solinum/ws/soap/client/soap/client/App.java
+++ src/main/java/fr/solinum/ws/soap/client/soap/client/App.java
@@ -1,13 +1,33 @@
 package fr.solinum.ws.soap.client.soap.client;
 
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPException;
+
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		// System.out.println( "Hello World!" );
+
+		SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory(
+				MessageFactory.newInstance());
+		messageFactory.afterPropertiesSet();
+
+		WebServiceTemplate webServiceTemplate = new WebServiceTemplate(
+				messageFactory);
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setContextPath("fr.solinum.ws.soap.client.soap.client.in");
+		marshaller.afterPropertiesSet();
+
+		webServiceTemplate.setMarshaller(marshaller);
+		webServiceTemplate.afterPropertiesSet();
+		
+		System.out.println(webServiceTemplate);
+	}
 }
---
+++ src/main/java/fr/solinum/ws/soap/client/soap/client/in/Request.java
@@ -0,0 +1,8 @@
+package fr.solinum.ws.soap.client.soap.client.in;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Request {
+
+}
